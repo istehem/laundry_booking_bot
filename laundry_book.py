267c3b37379db62c3242b6f527caae9d0c27e375
Oskar Ingemarsson <oskari@student.chalmers.se>
2014-07-02 01:39:14 +0200

    login works! Some errors with booking though

--- laundry_book.py
+++ laundry_book.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 import os
 import urllib
@@ -20,48 +22,52 @@ PANEL_ID=48033
 TRESH = 100
 
 BOOK_URL="https://tvatta.sgsstudentbostader.se/wwwashwait.aspx"
+CALENDAR_URL='https://tvatta.sgsstudentbostader.se/wwwashcalendar.aspx'
 LOGIN_URL="https://marknad.sgsstudentbostader.se/API/Service/AuthorizationServiceHandler.ashx"
+LOGIN_URL="https://www.sgsstudentbostader.se/Assets/Handlers/Momentum.ashx"
+COOKIE_FILE="cookie.txt"
+
+HEADERS = {
+            'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0 Iceweasel/29.0.1',
+            'Accept' :  '*/*',
+            'Accept-Encoding' : 'utf-8',
+
+          }
 
 def main():
-    cj = doLogin()
-#    book(cj)
-#    f = opener.open(LOGIN_URL)
-#    if f.read() == "Logged in":
-#        print "already loggid in"
-#        print "trying to book shifts"
-#    else:
-#        doLogin()
-    #doLogin()
-#    if book():
-#        print "booked a shift"
-#    else:
-#        print "booking failed"
+    opener = create_opener()
+    opener = hack_login(opener)
+    #try_to_book(opener,getDate(3),7)
+    data = get_calendar(opener,0)
+    #data = get_calendar(opener,0)
+    print data.read()
 
-def book(cj):
+def book(opener):
     for i in range(0,TRESH):
         date = getDate(i)
         for interval in range(0,7):
-            if try_to_book(cj,date,interval):
+            if try_to_book(opener,date,interval):
                 return True
     return False
 
-def try_to_book (cj,date,interval):
+def try_to_book (opener,date,interval):
     if interval < 0 or interval > 7:
         #invalid interval
         return False
     values = { 'command'    : 'book',
                'PanelId'    : str(PANEL_ID),
-               'TypeId'     : TYPE_ID,
+               'TypeId'     : str(TYPE_ID),
                'GroupId'    : str(GROUP_ID),
                'Date'       : date,
-               'IntervalId' : str(interval)
+               'IntervalId' : str(interval),
+               'NextPage'   : ''
             }
-    data = urllib.urlencode(values)
-    req = urllib2.Request(BOOK_URL, data)
-    cj.add_cookie_header(req)
-    response = urllib2.urlopen(req)
-    the_page = response.read()
-    print the_page
+    url_values = urllib.urlencode(values)
+    full_url = BOOK_URL + '?' + url_values
+    data = opener.open(full_url)
+    print full_url
+    data = get_calendar(opener,0)
+    #print full_url
     return True
 
 #return a date string using format "yyyy-mm-dd"
@@ -75,70 +81,45 @@ def getDate(i):
 def getInterval():
     return 0
 
-def doLogin():
-    headers = {
-            'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0 Iceweasel/29.0.1',
-            'Accept' :  '*/*',
-            'Accept-Encoding' : 'utf-8',
-            }
-            #'Cookie'  : 'ASP.NET_SessionId=azjhqx3iosd2ixf4mwynd2cq; _ga=GA1.2.167470743.1403793458',
 
 
-    values = {'syndicateNo' : '1',
-              'syndicateObjectMainGroupNo' : '1',
-              'username' : '122182',
-              'password' : '3248',
-              'Method'   : 'APILoginSGS',
-              'callback' : 'jsonp1403793470251'}
+def create_opener():
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders = HEADERS.items()
+    urllib2.install_opener(opener)
+    return opener
+
+#Security hole, allows this
+def hack_login(opener):
+    values = {'isresident'    : 'true',
+              'loggedin'      : 'true',
+              'customerid'    : '122182',
+              'tvattstuga'    : 'true',
+              'customer_name' : '503004325'
+             }
     url_values = urllib.urlencode(values)
     full_url = LOGIN_URL + '?' + url_values
-    cj = cookielib.FileCookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    opener.addheaders = headers.items()
-    #user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0 Iceweasel/29.0.1"
-    #opener.addheaders = [('User-Agent',user_agent)]
-    #req = urllib2.Request(full_url,headers=headers)
-    #req = urllib2.Request(full_url,header)
-    try:
-        data = opener.open(full_url)
-        #data = urllib2.urlopen(req)
-        contents = data.read()
-        print data.info()
-        print contents
-    except urllib2.HTTPError, e:
-        contents = e.read()
-        print e.geturl()
-        exit(1)
-    print cj
-    return cj
-#req = urllib2.urlopen("https://marknad.sgsstudentbostader.se/API/Service/AuthorizationServiceHandler.ashx?&syndicateNo=1&syndicateObjectMainGroupNo=1&username=122182&password=3248&Method=APILoginSGS&callback=jsonp140379347025")
-    #print res.read()
-#    isresident='true'
-#    customerid=USER
-#    loggedin='true'
-#    customerstatus='1'
-#    laundry='true'
-#    customerName='hej'
-#    laundry='trams'
-#    token=PASS
-#    gets = "isresident=" + isresident + \
-#           "&customerid=" + customerid + \
-#           "&loggedin=" + loggedin + \
-#           "&customerstatus=" + customerstatus + \
-#           "&tvattstuga=" + laundry + \
-#           "&customer_name=" + customerName + \
-#           "&token=" + token
-#    os.system("curl " + LOGIN_URL + '?' + gets)
-#
+    opener.open(full_url)
+    data = opener.open('https://www.sgsstudentbostader.se/ext_gw.aspx?module=wwwash&lang=se?loggedin=true')
+    return opener
+
+
+def login(opener):
+    pass
+
+def get_calendar(opener,week_offset):
+    values = {
+            'panelId'    : str(PANEL_ID),
+            'weekOffset' : str(week_offset),
+            'type'       : str(TYPE_ID),
+            'group'      : str(GROUP_ID)
+             }
+    url_values = urllib.urlencode(values)
+    full_url = CALENDAR_URL + '?' + url_values
+    data = opener.open(full_url)
+    return data
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-#https://tvatta.sgsstudentbostader.se/wwwashwait.aspx?command=book&PanelId=48033&TypeId=260685&GroupId=267946&Date=2014-06-03&IntervalId=7
-
