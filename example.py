3883d5090967642dd42d965d06e6f83a53a2a8c8
Oskar Ingemarsson <oskar.ingemarsson@gmail.com>
2014-07-16 22:26:59 +0200

    minor change + new doc

--- example.py
+++ example.py
@@ -13,35 +13,35 @@ class MyBot(SGS_bot):
         d = self.get_calendar(0)
         terminate = False 
         while True:
-            if terminate:
-                break
-            for weekday in range(0,6):
+            while True:
                 if terminate:
                     break
-                for shift in range (0,8):
-                    if self.is_free_shift(week_offset, weekday,shift,d):
-                        date = self.get_date_from_wd(week_offset,weekday)
-                        bs = self.get_booked_shift()
-                        if bs != None:
-                            if bs['date'] <= date and bs['interval'] <= shift:
-                               print "already has the earliest shift booked\n"
-                               terminate = True
-                               break
-                            else:
-                               self.try_to_unbook(bs['date'],bs['interval'])
-                        elif self.try_to_book(date, shift):
-                            print "successfully booked shift " + str(shift) + " at " + date 
-                            print "\n"
-                        else:
-                            print "unable to book shift " + str(shift) + " at " + date 
-                            print "\n"
-                        terminate = True
+                for weekday in range(0,6):
+                    if terminate:
                         break
-            week_offset = week_offset + 1
-
-        
-        d = self.get_calendar(week_offset-1)
-        self.print_calendar(d)
+                    for shift in range (0,8):
+                        if self.is_free_shift(week_offset, weekday,shift,d):
+                            date = self.get_date_from_wd(week_offset,weekday)
+                            bs = self.get_booked_shift()
+                            if bs != None:
+                                if bs['date'] <= date and bs['interval'] <= shift:
+                                   print "already has the earliest shift booked\n"
+                                   terminate = True
+                                   break
+                                else:
+                                   self.try_to_unbook(bs['date'],bs['interval'])
+                            elif self.try_to_book(date, shift):
+                                print "successfully booked shift " + str(shift) + " at " + date 
+                                print "\n"
+                            else:
+                                print "unable to book shift " + str(shift) + " at " + date 
+                                print "\n"
+                            terminate = True
+                            break
+                week_offset = week_offset + 1
+            d = self.get_calendar(week_offset-1)
+            self.print_calendar(d)
+            time.sleep(3600)
     def object_number(self):
         return 503004325
     def user_number(self):
